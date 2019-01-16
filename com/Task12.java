9b3bb025bec333447cd7ee94c5cf9ce76a4c1f3b
VitaliyPerelayko <VitaliyPerelayko@users.noreply.github.com>
2019-01-16 16:33:28 +0300

    task12

---
+++ com/Task12.java
@@ -0,0 +1,90 @@
+package com;
+
+import javax.imageio.ImageIO;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class Task12 extends HttpServlet {
+    private static long countOfUsers;
+
+    private static final long serialVersionUID=1L;
+    //Путь к файлу, где храниться количество посещений
+    private static final String PATH="E:\\Работа\\Віталюга\\It-academi\\Servlet\\src\\main\\resources\\countOfUsers.dat";
+
+    // При инициализации загружаем из файла счётчик посещений
+    public void init(){
+        DataInputStream stream=null;
+        try  {
+            stream = new DataInputStream(
+                    new BufferedInputStream(new FileInputStream(PATH)));
+            countOfUsers=stream.readLong();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("image/jpeg");
+        // увеличиваем счётчик пользователей (Задание 9)
+        countOfUsers++;
+
+        BufferedImage image = new  BufferedImage(800,200,BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setFont(new Font("Monospaced",Font.ITALIC,48));
+        graphics.setColor(Color.YELLOW);
+        graphics.drawString("You are "+countOfUsers+"th visitor",100,100);
+
+        ImageIO.write(image,"jpeg",response.getOutputStream());
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+
+    // При закрытии записываеи в файл счётчик посещений.
+    public void destroy(){
+        DataOutputStream stream=null;
+        try  {
+            stream = new DataOutputStream(
+                    new BufferedOutputStream(new FileOutputStream(PATH)));
+            stream.writeLong(countOfUsers);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
